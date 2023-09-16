87cebaa6cc7a3783ce9054888b331943ec401a03
srushti patel <srushtipatel2643@gmail.com>
2023-09-16 09:47:04 +0530

    16-09-2023

---
+++ math_function.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Ceil Number : " << ceil(7.5) << endl;
+    cout <<"Ceil Number : " << ceil(7.2) << endl;
+    cout <<"Ceil Number : " << ceil(7.9) << endl;
+    cout <<"Ceil Number : " << ceil(7.6) << endl;
+
+    cout <<"Floor Number : " << floor(7.2) << endl;
+    cout <<"Floor Number : " << floor(7.5) << endl;
+    cout <<"Floor Number : " << floor(7.9) << endl;
+    cout <<"Floor Number : " << floor(7.6) << endl;
+
+    cout <<"Round Number : " << round(7.6) << endl;
+    cout <<"Round Number : " << round(8.4) << endl;
+    cout <<"Round Number : " << round(7.4) << endl;
+    cout <<"Round Number : " << round(8.6) << endl;
+
+    cout <<"sqrt Number : " << sqrt(25) << endl;
+    cout <<"sqrt Number : " << sqrt(8) << endl;
+    cout <<"sqrt Number : " << sqrt(12) << endl;
+    cout <<"sqrt Number : " << sqrt(512) << endl;
+
+    cout <<"Power Number : " << pow(7,6) << endl;
+    cout <<"Power Number : " << pow(6,2) << endl;
+    cout <<"Power Number : " << pow(4,5) << endl;
+    cout <<"Power Number : " << pow(9,3) << endl;
+
+    cout <<"abs Number : " << abs(-76) << endl;
+    cout <<"abs Number : " << abs(-96) << endl;
+    cout <<"abs Number : " << abs(77) << endl;
+    cout <<"abs Number : " << abs(-65) << endl;
+}
---
+++ ope_ol_un_sphere.cpp
@@ -0,0 +1,38 @@
+// operator overloding in unary operator find to sphere area 
+
+#include <iostream>
+
+using namespace std;
+class sphere
+
+{
+int radius;
+
+public:
+void setdata(int ra)
+{
+    radius = ra;
+}
+sphere operator--(int)
+{
+    sphere s;
+    s.radius = radius--;
+   
+    return s;
+}
+int getdata()
+{
+    return 4 * 3.14 * radius * radius ;
+}
+};
+
+int main ()
+{
+    sphere sphere1,sphere2;
+
+    sphere1.setdata(10);
+    cout << "Sphere 1 is :- " << sphere1.getdata() << endl;
+
+    sphere1--;
+    cout << "Sphere 2 is :- " << sphere1.getdata() << endl;
+}
